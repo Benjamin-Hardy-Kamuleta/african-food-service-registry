dbc67d406997c7a8bcd037eaeffdeb46a30f1fa3
Benjamin-Hardy-Kamuleta <devkamuleta@gmail.com>
2024-03-12 00:38:06 +0100

    update config

--- src/main/java/com/hkbusiness/africanfoodserviceregistry/AfricanFoodServiceRegistryApplication.java
+++ src/main/java/com/hkbusiness/africanfoodserviceregistry/AfricanFoodServiceRegistryApplication.java
@@ -2,8 +2,10 @@ package com.hkbusiness.africanfoodserviceregistry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class AfricanFoodServiceRegistryApplication {
 
 	public static void main(String[] args) {
