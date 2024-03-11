6d46679ea2dc75374294fb96292da01134680449
Benjamin-Hardy-Kamuleta <devkamuleta@gmail.com>
2024-03-11 17:39:44 +0100

    initial code

---
+++ src/main/java/com/hkbusiness/africanfoodserviceregistry/AfricanFoodServiceRegistryApplication.java
@@ -0,0 +1,13 @@
+package com.hkbusiness.africanfoodserviceregistry;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfricanFoodServiceRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AfricanFoodServiceRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hkbusiness/africanfoodserviceregistry/AfricanFoodServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hkbusiness.africanfoodserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfricanFoodServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
